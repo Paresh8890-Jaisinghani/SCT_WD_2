b44c720897c06f67eceefcfacda87f299ea9b17c
Paresh8890-Jaisinghani <136781047+Paresh8890-Jaisinghani@users.noreply.github.com>
2024-08-05 23:53:12 +0530

    css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import React from 'react';
+import { FaPlayCircle } from "react-icons/fa";
+import { GrPowerReset } from "react-icons/gr";
+import { PiFlagPennantFill } from "react-icons/pi";
+
 
 function App() {
   return (
     <div className="App">
       <h1>StopWatch</h1>
+      <div className="timerbox">
+        {/* for timer */}
+        <div className="timer">
+          
+        </div>
+        {/* for lap time */}
+        <div className="lap">
+        <button> <PiFlagPennantFill/></button>
+        </div>
+        {/* for starting the time */}
+        <div className="start">
+         <button><FaPlayCircle/></button> 
+        </div>
+
+        {/* for reset */}
+        <div className="reset">
+          <button><GrPowerReset/></button>
+        </div>
+      </div>
     </div>
   );
 }
